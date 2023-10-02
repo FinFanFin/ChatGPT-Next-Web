8dac0076ed492766a91f84d9b0b9894cb605a03f
ppppppbz <516988422@qq.com>
2023-10-02 11:01:20 +0800

    rename

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,11 +145,11 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          AI Chat
         </div>
-        <div className={styles["sidebar-sub-title"]}>
+        {/* <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
-        </div>
+        </div> */}
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
@@ -206,11 +206,11 @@ export function SideBar(props: { className?: string }) {
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
           </div>
-          <div className={styles["sidebar-action"]}>
+          {/* <div className={styles["sidebar-action"]}>
             <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
-          </div>
+          </div> */}
         </div>
         <div>
           <IconButton
